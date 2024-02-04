7ed9e5fe8ddb9cb5691f44913d914226ee104f58
Tom Jones <94535525+ThomasJ0nes@users.noreply.github.com>
2024-02-04 20:37:17 +0000

    Move files outside of folder for deployment

---
+++ app/page.tsx
@@ -0,0 +1,52 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import { DynamicWidget } from "../lib/dynamic";
+import CTACards from "@/components/ctaCards";
+
+export default function Home() {
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <h1 className={title()}>Report&nbsp;</h1>
+          <h1 className={title({ color: "violet" })}>scientific&nbsp;</h1>
+          <br />
+          <h1 className={title()}>errors anonymously with ease 🎭</h1>
+          <h2 className={subtitle({ class: "mt-4" })}>
+            We understand that reporting scientific errors can sometimes lead to
+            backlash, so we provide a safe and secure environment for you to
+            contribute to the improvement of scientific research.
+          </h2>
+        </div>
+
+        <div className="flex gap-3">
+          <DynamicWidget
+            innerButtonComponent={<button>Get Started</button>}
+          ></DynamicWidget>
+          <Link
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href="/about"
+          >
+            Learn More
+          </Link>
+        </div>
+
+        <div className="mt-8">
+          <Snippet hideSymbol hideCopyButton variant="flat">
+            <span>
+              What to help improve zkCert{" "}
+              <Code color="primary">Contribute </Code>
+            </span>
+          </Snippet>
+        </div>
+      </section>
+
+      <CTACards />
+    </>
+  );
+}
---
+++ components/ctaCards.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Image } from "@nextui-org/image";
+
+const CTACards = () => {
+  return (
+    <div className="grid grid-cols-3 mt-10">
+      {" "}
+      <Card className="max-w-[400px]">
+        <CardHeader className="flex gap-3">
+          <Image
+            alt="nextui logo"
+            height={40}
+            radius="sm"
+            src="https://img.icons8.com/?size=64&id=52536&format=png"
+            width={40}
+          />
+          <div className="flex flex-col">
+            <p className="text-md font-semibold">Effortless Error Reporting</p>
+          </div>
+        </CardHeader>
+
+        <CardBody>
+          <p>
+            Submit reports with ease and confidence, knowing that your identity
+            will remain anonymous.
+          </p>
+        </CardBody>
+
+        <CardFooter></CardFooter>
+      </Card>
+      <Card className="max-w-[400px]">
+        <CardHeader className="flex gap-3">
+          <Image
+            alt="nextui logo"
+            height={40}
+            radius="sm"
+            src="https://img.icons8.com/?size=64&id=123737&format=png"
+            width={40}
+          />
+          <div className="flex flex-col">
+            <p className="text-md font-semibold">
+              Verified Academic Credentials
+            </p>
+          </div>
+        </CardHeader>
+
+        <CardBody>
+          <p>
+            Our platform offers a secure way to verify academic degrees without
+            compromising user privacy. We do this using Zero-Knowledge Proofs.
+          </p>
+        </CardBody>
+
+        <CardFooter></CardFooter>
+      </Card>
+      <Card className="max-w-[400px]">
+        <CardHeader className="flex gap-3">
+          <Image
+            alt="nextui logo"
+            height={40}
+            radius="sm"
+            src="https://img.icons8.com/?size=64&id=44045&format=png"
+            width={40}
+          />
+          <div className="flex flex-col">
+            <p className="text-md font-semibold">
+              {" "}
+              Search our database of sceintific papers
+            </p>
+          </div>
+        </CardHeader>
+
+        <CardBody>
+          <p>
+            {" "}
+            Search and browse our database of sceintific papers, read comment
+            and report errors anonymously.
+          </p>
+        </CardBody>
+
+        <CardFooter></CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default CTACards;
