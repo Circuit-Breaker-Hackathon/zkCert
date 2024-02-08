173464a918bc554434186eb466d9ea0b7ebc9b27
Tom Jones <94535525+ThomasJ0nes@users.noreply.github.com>
2024-02-08 14:02:25 +0000

    Article page design and comment section design.

---
+++ zkcert-web/components/commentSection.tsx
@@ -0,0 +1,353 @@
+"use client";
+import React from "react";
+import { Avatar } from "@nextui-org/avatar";
+import { Button } from "@nextui-org/button";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+} from "@nextui-org/dropdown";
+import { Textarea } from "@nextui-org/input";
+const CommentSection = () => {
+  return (
+    <section className="py-8 lg:py-16 antialiased">
+      <div className="max-w-2xl mx-auto px-4">
+        <div className="flex justify-between items-center mb-6">
+          <h2 className="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">
+            Discussion (20)
+          </h2>
+        </div>
+        <form className="mb-6">
+          <label className="sr-only">Your comment</label>
+          <Textarea
+            isRequired
+            label="Comment"
+            labelPlacement="outside"
+            placeholder="Write your comment here..."
+            className="max-w-full mb-4"
+          />
+
+          <Button color="secondary">Post Comment</Button>
+        </form>
+        <article className="p-6 text-base bg-white rounded-lg dark:bg-gray-900 mb-2">
+          <footer className="flex justify-between items-center mb-2">
+            <div className="flex items-center">
+              <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+                <Avatar
+                  classNames={{
+                    base: "bg-gradient-to-br from-purple-400 to-purple-700 mr-2 w-6 h-6 rounded-full",
+                    icon: "text-black/80",
+                  }}
+                  showFallback
+                  src="https://images.unsplash.com/broken"
+                />
+                Anonymous
+              </p>
+              <p
+                className="text-sm text-gray-600 dark:text-gray-400"
+                title="February 8th, 2022"
+              >
+                Feb. 8, 2022
+              </p>
+              <Dropdown>
+                <DropdownTrigger>
+                  <Button variant="light">
+                    {" "}
+                    <svg
+                      className="w-4 h-4"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 16 3"
+                    >
+                      <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                    </svg>
+                  </Button>
+                </DropdownTrigger>
+                <DropdownMenu
+                  aria-label="Action event example"
+                  onAction={(key) => alert(key)}
+                >
+                  <DropdownItem
+                    key="report"
+                    className="text-danger"
+                    color="danger"
+                  >
+                    Report Comment 🚩
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </div>
+          </footer>
+          <p className="text-gray-500 dark:text-gray-400">
+            Very straight-to-point article. Really worth time reading. Thank
+            you! But tools are just the instruments for the UX designers. The
+            knowledge of the design tools are as important as the creation of
+            the design strategy.
+          </p>
+          <div className="flex items-center mt-4 space-x-4">
+            <button
+              type="button"
+              className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium"
+            >
+              <svg
+                className="mr-1.5 w-3.5 h-3.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 18"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
+                />
+              </svg>
+              Reply
+            </button>
+          </div>
+        </article>
+        <article className="p-6 mb-3 ml-6 lg:ml-12 text-base bg-gray-200 rounded-lg dark:bg-gray-700">
+          <footer className="flex justify-between items-center mb-2">
+            <div className="flex items-center">
+              <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+                <Avatar
+                  classNames={{
+                    base: "bg-gradient-to-br from-purple-400 to-purple-700 mr-2 w-6 h-6 rounded-full",
+                    icon: "text-black/80",
+                  }}
+                  showFallback
+                  src="https://images.unsplash.com/broken"
+                />
+                Anonymous
+              </p>
+              <p className="text-sm text-gray-600 dark:text-gray-400">
+                <time title="February 12th, 2022">Feb. 12, 2022</time>
+              </p>
+            </div>
+            <Dropdown>
+              <DropdownTrigger>
+                <Button variant="light">
+                  {" "}
+                  <svg
+                    className="w-4 h-4"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 16 3"
+                  >
+                    <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                  </svg>
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                aria-label="Action event example"
+                onAction={(key) => alert(key)}
+              >
+                <DropdownItem key="edit">Edit Comment</DropdownItem>
+                <DropdownItem
+                  key="delete"
+                  className="text-danger"
+                  color="danger"
+                >
+                  Delete Comment
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </footer>
+          <p className="text-gray-500 dark:text-gray-400">
+            Much appreciated! Glad you liked it ☺️
+          </p>
+          <div className="flex items-center mt-4 space-x-4">
+            <button
+              type="button"
+              className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium"
+            >
+              <svg
+                className="mr-1.5 w-3.5 h-3.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 18"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
+                />
+              </svg>
+              Reply
+            </button>
+          </div>
+        </article>
+        <article className="p-6 text-base bg-white rounded-lg dark:bg-gray-900 mb-2">
+          <footer className="flex justify-between items-center mb-2">
+            <div className="flex items-center">
+              <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+                <Avatar
+                  classNames={{
+                    base: "bg-gradient-to-br from-purple-400 to-purple-700 mr-2 w-6 h-6 rounded-full",
+                    icon: "text-black/80",
+                  }}
+                  showFallback
+                  src="https://images.unsplash.com/broken"
+                />
+                Anonymous
+              </p>
+              <p
+                className="text-sm text-gray-600 dark:text-gray-400"
+                title="February 8th, 2022"
+              >
+                Feb. 8, 2022
+              </p>
+              <Dropdown>
+                <DropdownTrigger>
+                  <Button variant="light">
+                    {" "}
+                    <svg
+                      className="w-4 h-4"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 16 3"
+                    >
+                      <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                    </svg>
+                  </Button>
+                </DropdownTrigger>
+                <DropdownMenu
+                  aria-label="Action event example"
+                  onAction={(key) => alert(key)}
+                >
+                  <DropdownItem
+                    key="report"
+                    className="text-danger"
+                    color="danger"
+                  >
+                    Report Comment
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </div>
+          </footer>
+          <p className="text-gray-500 dark:text-gray-400">
+            Very straight-to-point article. Really worth time reading. Thank
+            you! But tools are just the instruments for the UX designers. The
+            knowledge of the design tools are as important as the creation of
+            the design strategy.
+          </p>
+          <div className="flex items-center mt-4 space-x-4">
+            <button
+              type="button"
+              className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium"
+            >
+              <svg
+                className="mr-1.5 w-3.5 h-3.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 18"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
+                />
+              </svg>
+              Reply
+            </button>
+          </div>
+        </article>
+        <article className="p-6 text-base bg-white rounded-lg dark:bg-gray-900 mb-2">
+          <footer className="flex justify-between items-center mb-2">
+            <div className="flex items-center">
+              <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+                <Avatar
+                  classNames={{
+                    base: "bg-gradient-to-br from-purple-400 to-purple-700 mr-2 w-6 h-6 rounded-full",
+                    icon: "text-black/80",
+                  }}
+                  showFallback
+                  src="https://images.unsplash.com/broken"
+                />
+                Anonymous
+              </p>
+              <p
+                className="text-sm text-gray-600 dark:text-gray-400"
+                title="February 8th, 2022"
+              >
+                Feb. 8, 2022
+              </p>
+              <Dropdown>
+                <DropdownTrigger>
+                  <Button variant="light">
+                    {" "}
+                    <svg
+                      className="w-4 h-4"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 16 3"
+                    >
+                      <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                    </svg>
+                  </Button>
+                </DropdownTrigger>
+                <DropdownMenu
+                  aria-label="Action event example"
+                  onAction={(key) => alert(key)}
+                >
+                  <DropdownItem
+                    key="report"
+                    className="text-danger"
+                    color="danger"
+                  >
+                    Report Comment
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </div>
+          </footer>
+          <p className="text-gray-500 dark:text-gray-400">
+            Very straight-to-point article. Really worth time reading. Thank
+            you! But tools are just the instruments for the UX designers. The
+            knowledge of the design tools are as important as the creation of
+            the design strategy.
+          </p>
+          <div className="flex items-center mt-4 space-x-4">
+            <button
+              type="button"
+              className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium"
+            >
+              <svg
+                className="mr-1.5 w-3.5 h-3.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 18"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
+                />
+              </svg>
+              Reply
+            </button>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default CommentSection;
